ac531c61a93824e5375286aa40c92b1457f8317a
vaibhav sukale <104653559+itsvaibhavcoder@users.noreply.github.com>
2024-01-06 23:52:25 +0530

    Add files via upload

---
+++ 5_Array_of_object.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Employee{
+    int id;
+    int salary;
+
+    public:
+    void setId(void){
+        salary = 22;
+        cout<<"Enter the id of employee"<<endl;
+        cin>>id;
+    }
+
+    void getId(void){
+        cout<<"The id of this employee is "<<id<<endl;
+    }
+};
+int main()
+{
+Employee fb[4];
+for(int i=0; i<4; i++){
+    fb[i].setId();
+    fb[i].getId();
+}
+ return 0;
+}
